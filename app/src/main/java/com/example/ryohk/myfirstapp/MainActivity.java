66e3b8d36956ebecfd9e63d434195ffe29a46f96
ryohk <r-koyama@ist.osaka-u.ac.jp>
2018-07-13 12:45:04 +0900

    test

--- app/src/main/java/com/example/ryohk/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/ryohk/myfirstapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //test commit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
